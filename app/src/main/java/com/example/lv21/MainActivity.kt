75117f6ecb902acc761955861c6a803676d52501
kova <bornakovacevic5@gmail.com>
2023-12-29 11:13:46 +0100

    Lv3 Done (dodane kontorle,back,home..)

--- app/src/main/java/com/example/lv21/MainActivity.kt
+++ app/src/main/java/com/example/lv21/MainActivity.kt
@@ -1,78 +1,108 @@
- package com.example.lv21
+package com.example.yourpackage
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lv21.R
 
- import android.os.Bundle
- import android.view.View
- import android.widget.Button
- import android.widget.TextView
- import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
 
- class MainActivity : AppCompatActivity() {
+    private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var backButton: Button
+    private lateinit var resultTextView: TextView
 
-     private lateinit var questionTextView: TextView
-     private lateinit var trueButton: Button
-     private lateinit var falseButton: Button
-     private lateinit var nextButton: Button
-     private lateinit var resultTextView: TextView
+    private val questions = listOf(
+        R.string.question_1,
+        R.string.question_2,
+        R.string.question_3,
+        R.string.question_4,
+        R.string.question_5
+    )
 
-     private val questions = listOf(
-         R.string.question_1,
-         R.string.question_2,
-         R.string.question_3,
-         R.string.question_4,
-         R.string.question_5
-     )
+    private var currentQuestionIndex = 0
 
-     private var currentQuestionIndex = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
+        questionTextView = findViewById(R.id.questionTextView)
+        trueButton = findViewById(R.id.trueButton)
+        falseButton = findViewById(R.id.falseButton)
+        nextButton = findViewById(R.id.nextButton)
+        backButton = findViewById(R.id.backButton)
+        resultTextView = findViewById(R.id.resultTextView)
 
-         questionTextView = findViewById(R.id.questionTextView)
-         trueButton = findViewById(R.id.trueButton)
-         falseButton = findViewById(R.id.falseButton)
-         nextButton = findViewById(R.id.nextButton)
-         resultTextView = findViewById(R.id.resultTextView)
+        displayQuestion()
 
-         displayQuestion()
+        trueButton.setOnClickListener { checkAnswer(true) }
+        falseButton.setOnClickListener { checkAnswer(false) }
+        nextButton.setOnClickListener { nextQuestion() }
+        backButton.setOnClickListener { previousQuestion() }
 
-         trueButton.setOnClickListener { checkAnswer(true) }
-         falseButton.setOnClickListener { checkAnswer(false) }
-         nextButton.setOnClickListener { nextQuestion() }
-     }
+        // Logiranje promjena u životnom ciklusu
+        Log.i("Lifecycle", "onCreate")
+    }
 
-     private fun displayQuestion() {
-         questionTextView.setText(questions[currentQuestionIndex])
-         trueButton.isEnabled = true
-         falseButton.isEnabled = true
-         nextButton.visibility = View.GONE
-         resultTextView.text = ""
-     }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle", "onStart")
+    }
 
-     private fun checkAnswer(userAnswer: Boolean) {
-         val correctAnswer = when (currentQuestionIndex) {
-             0 -> true
-             1 -> true
-             2 -> true
-             3 -> false
-             4 -> false
-             else -> false
-         }
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "onResume")
+    }
 
-         if (userAnswer == correctAnswer) {
-             resultTextView.setText(R.string.correct_answer)
-         } else {
-             resultTextView.setText(R.string.incorrect_answer)
-         }
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "onPause")
+    }
 
-         trueButton.isEnabled = false
-         falseButton.isEnabled = false
-         nextButton.visibility = View.VISIBLE
-     }
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "onStop")
+    }
 
-     private fun nextQuestion() {
-         currentQuestionIndex = (currentQuestionIndex + 1) % questions.size
-         displayQuestion()
-     }
- }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle", "onRestart")
+    }
+
+    private fun displayQuestion() {
+        questionTextView.setText(questions[currentQuestionIndex])
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
+        nextButton.visibility = View.GONE
+        resultTextView.text = ""
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        // Implementacija provjere odgovora
+
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+        nextButton.visibility = View.VISIBLE
+    }
+
+    private fun nextQuestion() {
+        currentQuestionIndex = (currentQuestionIndex + 1) % questions.size
+        displayQuestion()
+    }
+
+    private fun previousQuestion() {
+        // Implementacija za povratak na prethodno pitanje
+        // Ovdje bi trebalo biti dodatno rukovanje kako bi se spriječilo
+        // izlazak izvan granica liste pitanja.
+    }
+}
